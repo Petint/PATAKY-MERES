7b16e5f2be07112dc541e86152de4844d9ba3c96
Petrás Bálint <petras.balint04@gmail.com>
2022-12-06 15:28:16 +0100

    Update washingmachine.py

--- PetintLib/washingmachine.py
+++ PetintLib/washingmachine.py
@@ -5,17 +5,24 @@ import json
 
 
 def clean(file_in: str):
+    """Turn saved waveforms into usable data"""
     with open(file_in, 'rt', encoding='utf-8') as du:
         data = du.read().splitlines()
     data = [n.split(',') for n in data]
     keys = [f[0] for f in data[:13]]
     vaules = [f[1] for f in data[:12]]
     waveform_data = [float(f[0]) for f in data[13:]]
-    vaules.append(waveform_data)
     for (i, v) in enumerate(vaules):
         try:
             vaules[i] = float(v)
         except (ValueError, TypeError):
             pass
-    d = {k: v for (k, v) in zip(keys, vaules)}
-    return json.dumps(d)
+    param = {k: v for (k, v) in zip(keys, vaules)}
+    return param, waveform_data
+
+
+def cal(param: dict, waveform: 'list[float]'):
+    """Extracts values from the data"""
+    time = [i*param['Horizontal Scale'] for i in range(int(param['Memory Length']))]
+    wf = [i*param['Vertical Scale'] for i in waveform]
+    return time, wf
